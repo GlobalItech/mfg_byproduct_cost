5531539a3bc780d5f49a0d102d38a5127998a86b
GlobalItech <30366468+GlobalItech@users.noreply.github.com>
2020-02-11 13:06:03 +0500

    Add files via upload

---
+++ mfg_byproduct_cost/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Finished goods and by-products costing(Manufacturing)",
+    "version": "0.1",
+    "author": "ITECH RESOURCES",
+    "category": "Manufacturing",
+    "description": """
+        This module will managed the Manufacturing plus costing,
+    """,
+    "depends": ['mrp','product', 'stock', 'resource','mrp_byproduct','mrp_account'],
+    "data": [
+             'views/shafi_work.xml',
+             'views/reports.xml',
+             ],
+             
+    "installable": True,
+    "auto_install": False,
+}
